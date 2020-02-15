10f83be42351ec96bb8d9cbb9fe9c95031630315
RafaelBackx <43341347+RafaelBackx@users.noreply.github.com>
2020-02-15 17:55:02 +0100

    added java and python icons

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ htmlIcon.addEventListener('mouseenter',ColorImage,false);
 htmlIcon.addEventListener('mouseleave',ChangeColor,false);
 
 function ColorImage(e){
-    console.log(e);
     e.target.childNodes[0].src = 'images/'+e.target.id + '-icon-colored.png';
     console.log(e.target.id + '-icon-colored.png');
 }
 
 function ChangeColor(e){
-    console.log(e);
     e.target.childNodes[0].src = 'images/'+e.target.id + '-icon.png';
     console.log(e.target.id + '-icon.png');
 }
